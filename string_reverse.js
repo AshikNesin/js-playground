b893b62f0878c17e5b4a8a3a7d521568c8c7d13e
Ashik Nesin <mail@ashiknesin.com>
2016-12-06 20:11:46 +0530

    Add String Reverse

---
+++ string_reverse.js
@@ -0,0 +1,6 @@
+message = 'find you will pain only go you recordings security the into if';
+// returns: 'if into the security recordings you go only pain will you find'
+
+const reverseMessage = (msg) => msg.split(" ").reverse().join(" ");
+
+console.log(reverseMessage(message))
